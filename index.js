c58d7eff33b7df36f4db0885f21a95023d489ce3
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-09 13:36:20 +0300

    commit tree 3d8dc1b46e3044ef386825c1f7cc6140aad26ee8

---
+++ index.js
@@ -0,0 +1,26 @@
+let scuberHq = 42;
+
+function distanceFromHqInBlocks(distance) {
+    return Math.abs(distance - scuberHq)
+}
+
+function distanceFromHqInFeet(distance) {
+    return (Math.abs(distance - scuberHq) * 264)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const rangeInFeet = Math.abs(destination - start) * 264
+    if (rangeInFeet <= 400) {
+        return 0
+    } else if (rangeInFeet > 400 && rangeInFeet <= 2000) {
+        return (rangeInFeet - 400) * 0.02
+    } else if (rangeInFeet > 2000 && rangeInFeet <= 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
